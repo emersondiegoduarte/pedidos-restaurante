eacece80efb145fa0583c4c3579e7bc45431c6c2
emerson.diego.duarte <emerson.diego.duarte@gmail.com>
2021-02-04 11:47:07 -0300

    ajsutes docker compose

--- backend/src/index.js
+++ backend/src/index.js
@@ -8,7 +8,13 @@ const app = express();
 const cors = require("cors");
 const port = process.env.PORT || 3001;
 
-mongoose.connect('mongodb://localhost:27017/pedidos-restaurante', {
+// mongoose.connect('mongodb+srv://diegodias:melinda2904@cluster0.jg2sm.mongodb.net/test', {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//     useFindAndModify: false
+// })
+
+mongoose.connect('mongodb://mongo:27017/pedidos-restaurante', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useFindAndModify: false
